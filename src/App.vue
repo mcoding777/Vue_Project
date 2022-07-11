5c757f19676011b4bc93b0ff7024a27f914d14ec
misun <mcoding777@naver.com>
2022-07-11 17:33:13 +0900

    feat: vue 반복문 사용

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <div>
-    <h1>Vue 원룸</h1>
-    <h4>XX 원룸</h4>
-    <p>{{price1}} 만원</p>
+  <div class="menu">
+    <a v-for="menu in menus" :key="menu">{{menu}}</a>
   </div>
-    <div>
-    <h4>OO 원룸</h4>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>Vue 원룸</h1>
+  <div>
+    <h4>{{products[0]}} 원룸</h4>
+    <p>50 만원</p>
+  </div>
+  <div>
+    <h4>{{products[1]}} 원룸</h4>
+    <p>{{price2}} 만원</p>
+  </div>
+  <div>
+    <h4>{{products.at(-1)}} 원룸</h4>
     <p>{{price2}} 만원</p>
   </div>
 </template>
@@ -16,8 +23,8 @@ export default {
   name: "App",
   data() {
     return {
-      price1: 60,
-      price2: 50,
+      products: ['역삼동', '강남동', '개포동'],
+      menus: ['Home', 'Products', 'About']
     }
   },
   components: {},
@@ -33,4 +40,16 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 0 100px;
+}
+
 </style>
